bbce5e3fcc9cb2cc1a6de429caed0f8308a84e80
WaterBean <gkstnqls9@gmail.com>
2022-08-18 15:36:22 +0900

    initial

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -5,7 +5,6 @@ from django.db import migrations, models
 import django.db.models.deletion
 import django.utils.timezone
 
-
 class Migration(migrations.Migration):
 
     initial = True
@@ -27,3 +26,4 @@ class Migration(migrations.Migration):
             ],
         ),
     ]
+
